061c950f49888bd4df6c2c6bce4091b0ab5e0e08
Siful Islam <sifulsmm@gmail.com>
2020-11-02 22:25:35 +0600

    files added 2

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -4,7 +4,7 @@ const Product = (props) => {
     return (
         <div style={{border: '1px solid red', marginBottom: '20px', padding: '10px'}}>
             <h5>{props.pd.name}</h5>
-            <button>Add to Cart</button>
+            <button> Add to Cart</button>
         </div>
     );
 };
